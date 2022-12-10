99a0e417968a4816dccf23369e24ae3be1fb9fec
Jonathan Rothberg <jonnyrpi@gmail.com>
2022-12-09 22:45:44 -0800

    Day 9 and 10

---
+++ aoc09/src/main.rs
@@ -0,0 +1,335 @@
+use std::{
+    collections::{HashMap, HashSet},
+    io::{self, Read},
+};
+
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    part1(&input)?;
+    part2(&input)?;
+
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+enum Command {
+    Up(i32),
+    Down(i32),
+    Left(i32),
+    Right(i32),
+}
+
+impl From<&str> for Command {
+    fn from(item: &str) -> Self {
+        let parts: Vec<&str> = item.split_whitespace().collect();
+        let n = parts[1].parse::<i32>().unwrap();
+        match parts[0] {
+            "U" => Self::Up(n),
+            "D" => Self::Down(n),
+            "L" => Self::Left(n),
+            "R" => Self::Right(n),
+            _ => panic!("unrecognized command: {:?}", parts),
+        }
+    }
+}
+
+fn part1(input: &str) -> Result<()> {
+    let commands: Vec<Command> = input.lines().map(|l| l.into()).collect();
+
+    let mut tail_positions = HashSet::new();
+    let mut tail_position: (i32, i32) = (0, 0);
+    let mut position: (i32, i32) = (0, 0);
+    let mut prev_poistion: (i32, i32) = position;
+    tail_positions.insert(tail_position);
+    for c in commands {
+        update_position(
+            &c,
+            &mut position,
+            &mut tail_position,
+            &mut tail_positions,
+            &mut prev_poistion,
+        )?;
+    }
+
+    println!("Tail Positions: {:?}", tail_positions.len());
+    // println!("Tail Positions: {:?}", tail_positions);
+
+    Ok(())
+}
+
+fn update_position(
+    c: &Command,
+    position: &mut (i32, i32),
+    tail_position: &mut (i32, i32),
+    tail_positions: &mut HashSet<(i32, i32)>,
+    prev_position: &mut (i32, i32),
+) -> Result<()> {
+    let n = match c {
+        Command::Up(n) => *n,
+        Command::Down(n) => *n,
+        Command::Left(n) => *n,
+        Command::Right(n) => *n,
+    };
+    for _ in 0..n {
+        *prev_position = *position;
+        let d = match c {
+            Command::Up(_) => {
+                *position = (position.0 - 1, position.1);
+                -1
+            }
+            Command::Down(_) => {
+                *position = (position.0 + 1, position.1);
+                1
+            }
+            Command::Left(_) => {
+                *position = (position.0, position.1 - 1);
+                -1
+            }
+            Command::Right(_) => {
+                *position = (position.0, position.1 + 1);
+                1
+            }
+        };
+
+        // check row and column for tail
+        if tail_position.0 == position.0 {
+            if (position.1 - tail_position.1).abs() > 1 {
+                tail_position.1 += d;
+            }
+        } else if tail_position.1 == position.1 {
+            if (position.0 - tail_position.0).abs() > 1 {
+                tail_position.0 += d;
+            }
+        } else {
+            // diagonal
+            let mut is_touching = false;
+            let mut test_pos = (tail_position.0 - 1, tail_position.1 + 1);
+            if test_pos.0 == position.0 && test_pos.1 == position.1
+            // && (tail_position.0 != position.0 && tail_position.1 != position.1)
+            {
+                is_touching = true;
+                // *tail_position = *position;
+            }
+
+            test_pos = (tail_position.0 + 1, tail_position.1 + 1);
+            if test_pos.0 == position.0 && test_pos.1 == position.1
+            // && (tail_position.0 != position.0 && tail_position.1 != position.1)
+            {
+                is_touching = true;
+                // *tail_position = *position;
+            }
+
+            test_pos = (tail_position.0 + 1, tail_position.1 - 1);
+            if test_pos.0 == position.0 && test_pos.1 == position.1
+            // && (tail_position.0 != position.0 && tail_position.1 != position.1)
+            {
+                is_touching = true;
+                // *tail_position = *position;
+            }
+
+            test_pos = (tail_position.0 - 1, tail_position.1 - 1);
+            if test_pos.0 == position.0 && test_pos.1 == position.1
+            // && (tail_position.0 != position.0 && tail_position.1 != position.1)
+            {
+                is_touching = true;
+                // *tail_position = *position;
+            }
+
+            if !is_touching {
+                *tail_position = *prev_position;
+            }
+        }
+
+        tail_positions.insert(*tail_position);
+    }
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct KnotPosition {
+    current: (i32, i32),
+    visited: HashSet<(i32, i32)>,
+}
+
+fn part2(input: &str) -> Result<()> {
+    let commands: Vec<Command> = input.lines().map(|l| l.into()).collect();
+
+    let mut tail_positions: HashMap<i32, KnotPosition> = HashMap::new();
+    // let mut tail_position: (i32, i32) = (0, 0);
+    let position: (i32, i32) = (0, 0);
+    let mut prev_poistion: HashMap<i32, (i32, i32)> = HashMap::new();
+    prev_poistion.insert(0, position);
+    for i in 0..10 {
+        let mut init = HashSet::new();
+        init.insert((0, 0));
+        tail_positions.insert(
+            i,
+            KnotPosition {
+                current: (0, 0),
+                visited: init,
+            },
+        );
+    }
+
+    for c in commands {
+        update_all_knots(
+            &c,
+            // &mut position,
+            // &mut tail_position,
+            &mut tail_positions,
+            &mut prev_poistion,
+        )?;
+    }
+
+    println!("Tail Positions (9): {:?}", tail_positions[&9].visited.len());
+    // println!("Tail Positions: {:?}", tail_positions);
+
+    Ok(())
+}
+
+fn update_all_knots(
+    c: &Command,
+    // positions: &mut HashMap<i32, (i32, i32)>,
+    // tail_position: &mut (i32, i32),
+    tail_positions: &mut HashMap<i32, KnotPosition>,
+    prev_position: &mut HashMap<i32, (i32, i32)>,
+) -> Result<()> {
+    let n = match c {
+        Command::Up(n) => *n,
+        Command::Left(n) => *n,
+        Command::Down(n) => *n,
+        Command::Right(n) => *n,
+    };
+    for _ in 0..n {
+        let prev_head = tail_positions[&0].current;
+        prev_position.insert(0, prev_head);
+        let d = match c {
+            Command::Up(_) => {
+                tail_positions.entry(0).and_modify(|p| {
+                    (*p).current.0 -= 1;
+                }); // (position.0 - 1, position.1);
+                -1
+            }
+            Command::Down(_) => {
+                //*position = (position.0 + 1, position.1);
+                tail_positions.entry(0).and_modify(|p| {
+                    (*p).current.0 += 1;
+                });
+                1
+            }
+            Command::Left(_) => {
+                //*position = (position.0, position.1 - 1);
+                tail_positions.entry(0).and_modify(|p| {
+                    (*p).current.1 -= 1;
+                });
+                -1
+            }
+            Command::Right(_) => {
+                // *position = (position.0, position.1 + 1);
+                tail_positions.entry(0).and_modify(|p| {
+                    (*p).current.1 += 1;
+                });
+                1
+            }
+        };
+
+        for k in 1..10 {
+            let prev = tail_positions[&k].current;
+
+            prev_position.insert(k, prev);
+
+            // check row and column for tail
+            if tail_positions[&(k - 1)].current.0 == tail_positions[&k].current.0 {
+                if (tail_positions[&k].current.1 - tail_positions[&(k - 1)].current.1).abs() > 1 {
+                    // tail_positions[&(k - 1)].current.1 += d;
+                    tail_positions.entry(k).and_modify(|s| {
+                        s.current.1 += d;
+                    });
+                }
+            } else if tail_positions[&(k - 1)].current.1 == tail_positions[&k].current.1 {
+                if (tail_positions[&(k - 1)].current.0 - tail_positions[&k].current.0).abs() > 1 {
+                    // tail_positions[&(k - 1)].current.0 += d;
+                    tail_positions.entry(k).and_modify(|s| {
+                        s.current.0 += d;
+                    });
+                }
+            } else {
+                // diagonal
+                let mut is_touching = false;
+                let mut test_pos = (
+                    tail_positions[&(k - 1)].current.0 - 1,
+                    tail_positions[&(k - 1)].current.1 + 1,
+                );
+                if test_pos.0 == tail_positions[&k].current.0
+                    && test_pos.1 == tail_positions[&k].current.1
+                // && (tail_position.0 != position.0 && tail_position.1 != position.1)
+                {
+                    is_touching = true;
+                    // *tail_position = *position;
+                }
+
+                test_pos = (
+                    tail_positions[&(k - 1)].current.0 + 1,
+                    tail_positions[&(k - 1)].current.1 + 1,
+                );
+                if test_pos.0 == tail_positions[&k].current.0
+                    && test_pos.1 == tail_positions[&k].current.1
+                // && (tail_position.0 != position.0 && tail_position.1 != position.1)
+                {
+                    is_touching = true;
+                    // *tail_position = *position;
+                }
+
+                test_pos = (
+                    tail_positions[&(k - 1)].current.0 + 1,
+                    tail_positions[&(k - 1)].current.1 - 1,
+                );
+                if test_pos.0 == tail_positions[&k].current.0
+                    && test_pos.1 == tail_positions[&k].current.1
+                // && (tail_position.0 != position.0 && tail_position.1 != position.1)
+                {
+                    is_touching = true;
+                    // *tail_position = *position;
+                }
+
+                test_pos = (
+                    tail_positions[&(k - 1)].current.0 - 1,
+                    tail_positions[&(k - 1)].current.1 - 1,
+                );
+                if test_pos.0 == tail_positions[&k].current.0
+                    && test_pos.1 == tail_positions[&k].current.1
+                // && (tail_position.0 != position.0 && tail_position.1 != position.1)
+                {
+                    is_touching = true;
+                    // *tail_position = *position;
+                }
+
+                if !is_touching {
+                    // *tail_position = prev_position[&k];
+                    // println!("Before: {:?}", tail_positions[&k].current);
+                    tail_positions.entry(k).and_modify(|s| {
+                        s.current = prev_position[&(k - 1)];
+                        s.visited.insert(s.current);
+                    });
+                    // println!("After: {:?}", tail_positions[&k].current);
+                    // tail_positions[&k].current = prev_position[&k];
+                }
+            }
+
+            if k == 9 {
+                println!("{:?}", tail_positions[&k].current);
+            }
+            // let prev_tail_pos = prev_position[&(k - 1)];
+            // tail_positions.entry(k).and_modify(|s| {
+            //     s.visited.insert(prev_tail_pos);
+            // });
+        }
+    }
+
+    Ok(())
+}
---
+++ aoc10/src/main.rs
@@ -0,0 +1,132 @@
+use std::io::{self, Read};
+
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    part1(&input)?;
+    part2(&input)?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Noop,
+    Addx(i32),
+}
+
+impl From<&str> for Instruction {
+    fn from(item: &str) -> Self {
+        let parts: Vec<&str> = item.split_whitespace().collect();
+        if parts.len() == 1 {
+            Self::Noop
+        } else {
+            let arg: i32 = parts[1].parse().unwrap();
+            Self::Addx(arg)
+        }
+    }
+}
+
+fn part1(input: &str) -> Result<()> {
+    let instructions: Vec<Instruction> = input.lines().map(|l| l.into()).collect();
+
+    let mut clock = 0;
+    let mut x = 1;
+    let mut did_twenty = false;
+    let mut signal = 0;
+    for i in instructions {
+        // println!("{:?}", i);
+        match i {
+            Instruction::Noop => {
+                clock += 1;
+            }
+            Instruction::Addx(v) => {
+                // clock += 2;
+                for _ in 0..2 {
+                    clock += 1;
+                    if !did_twenty && clock % 20 == 0 {
+                        did_twenty = true;
+                        signal += clock * x;
+                        // println!("{} * {} = {}", clock, x, signal);
+                    } else if did_twenty && (clock + 20) % 40 == 0 {
+                        signal += clock * x;
+                        // println!("{} * {} = {}", clock, x, signal);
+                    }
+                }
+                x += v;
+            }
+        }
+
+        if clock >= 220 {
+            break;
+        }
+
+        // clock += 1;
+    }
+
+    // println!("Clock: {}", clock);
+    // pintln!("X: {}", x);
+    println!("Part 1 Signal: {}", signal);
+    Ok(())
+}
+
+fn part2(input: &str) -> Result<()> {
+    let instructions: Vec<Instruction> = input.lines().map(|l| l.into()).collect();
+
+    let mut clock: usize = 0;
+    let mut x: i32 = 1;
+    // let mut did_twenty = false;
+    // let mut signal = 0;
+    let mut sprite: i32 = 1;
+    let mut pixels = vec![vec!['.'; 40]; 6];
+    for i in instructions {
+        // println!("{:?}", i);
+        match i {
+            Instruction::Noop => {
+                clock += 1;
+                let row = (clock / 40) as usize;
+                let col = clock % 40;
+                // println!("Sprite: {}, Clock: {}", sprite, clock);
+                if col >= (sprite) as usize && col < (sprite + 3) as usize {
+                    pixels[row][col] = '#';
+                }
+                if clock % 40 == 0 {
+                    // signal += clock * x;
+                    // println!("{} * {} = {}", clock, x, signal);
+                    // println!("Sprite: {}, Clock: {}", sprite, clock);
+                    println!("{:?}", pixels[row - 1]);
+                }
+            }
+            Instruction::Addx(v) => {
+                // clock += 2;
+                for _ in 0..2 {
+                    clock += 1;
+
+                    let row = (clock / 40) as usize;
+                    let col = clock % 40;
+                    // println!("Sprite: {}, {}, {}", sprite, row, col);
+                    if col >= sprite as usize && col < (sprite + 3) as usize {
+                        pixels[row][col] = '#';
+                    }
+                    if clock % 40 == 0 {
+                        // signal += clock * x;
+                        // println!("{} * {} = {}", clock, x, signal);
+                        // println!("Sprite: {}, Clock: {}", sprite, clock);
+                        println!("{:?}", pixels[row - 1]);
+                    }
+                }
+                x += v;
+                sprite = x;
+            }
+        }
+
+        if clock == 240 {
+            break;
+        }
+    }
+
+    Ok(())
+}
